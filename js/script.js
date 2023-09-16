4cc68b607c8c1ef0471f3005bcfe2b2cf78b0cc4
Hayk Barseghyan <hayk.barseghyan.2006@mail.ru>
2023-09-16 17:17:02 +0400

    Added start version

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let menuBtn = document.querySelector('.burger_ico');
+let menu = document.querySelector('.header_nav_links');
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+})
+
